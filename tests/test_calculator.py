57bc67e9f1c474f196d01be11280f9fe61d80bd3
Christopher Birnie-Browne <153604499+Git-Hub-Chris@users.noreply.github.com>
2026-03-01 00:25:05 -0700

    Create test_calculator.py with original content

---
+++ tests/test_calculator.py
@@ -0,0 +1,17 @@
+import unittest
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(1 + 1, 2)
+
+    def test_subtract(self):
+        self.assertEqual(2 - 1, 1)
+
+    def test_multiply(self):
+        self.assertEqual(2 * 2, 4)
+
+    def test_divide(self):
+        self.assertEqual(4 / 2, 2)
+
+if __name__ == '__main__':
+    unittest.main()
