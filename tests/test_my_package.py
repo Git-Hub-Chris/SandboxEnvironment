808d63a52a9a3686dd0e1736111d5f8f73f98f8f
Christopher Birnie-Browne <153604499+Git-Hub-Chris@users.noreply.github.com>
2026-03-05 20:25:56 -0700

    Add tests for my_package to include it in coverage

---
+++ tests/test_my_package.py
@@ -0,0 +1,14 @@
+import pytest
+
+from my_package.calculator import add, divide
+from my_package.hello import hello
+
+def test_my_package_hello() -> None:
+    assert hello() == "Hello, World!"
+
+def test_my_package_add() -> None:
+    assert add(1, 2) == 3
+
+def test_my_package_divide_by_zero() -> None:
+    with pytest.raises(ValueError, match="Cannot divide by zero"):
+        divide(1, 0)
